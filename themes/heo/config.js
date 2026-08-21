b0b6ae176d949852f78abff3dff5fa18cf1029e1
mo521982 <85850646+mo521982@users.noreply.github.com>
2026-08-21 16:08:37 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://www.2xinhui.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '关于博主Xinhui', url: 'https://www.2xinhui.com/about' }
   ],
 
   // 英雄区左右侧组件颠倒位置
